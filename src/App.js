2b31bd7b66f0696b73a05d9f14925fbf78625a29
Sandilya Kambhampati <kprsandilya@gmail.com>
2023-09-24 20:20:07 -0400

    main app built

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import "./input.css";
+import { useState } from 'react';
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <link href="/dist/output.css" rel="stylesheet"></link>
+        <p class="text-blue-600">
+          Yay! Test Worked!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Started learning React.
+        < MyButton />
       </header>
     </div>
   );
