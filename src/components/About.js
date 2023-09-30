3280a0468021d9eeef2064fed4a223148fdc9e18
Sandilya Kambhampati <kprsandilya@gmail.com>
2023-09-30 16:55:38 -0400

    Linking between pages working

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../App.css';
+import "../input.css";
+  
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+};
+  
+export default About;
