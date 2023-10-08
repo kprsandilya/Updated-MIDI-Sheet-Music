378f21ab6bcdea1a96f8c2e0ec3377d7cffc4ba8
Sandilya Kambhampati <kprsandilya@gmail.com>
2023-10-08 01:31:10 -0400

    This week's tasks are accomplished

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,6 +6,7 @@ import Footer from "./Footer.js";
 import HeroPattern from "./HeroPattern.js";
 import { useState } from 'react';
 import * as Tone from 'tone';
+import { parseArrayBuffer } from 'midi-json-parser';
 const { Midi } = require('@tonejs/midi');
 const synth = new Tone.Synth().toDestination();
 
@@ -32,8 +33,7 @@ function AudioButton() {
   );
 }
 
-function FileUploadPage(){
-	const [selectedFile, setSelectedFile] = useState();
+function FileUploadPage({ selectedFile, setSelectedFile, setFileJSON }){
 	const [isSelected, setIsSelected] = useState(false);
 
 	const changeHandler = (event) => {
@@ -41,26 +41,32 @@ function FileUploadPage(){
 		setIsSelected(true);
 	};
 
-	const handleSubmission = () => {
-    console.log(selectedFile);
-	};
+  const handleFileChange = (event) => {
+    if (selectedFile) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const arrayBuffer = reader.result;
+        parseArrayBuffer(arrayBuffer).then((json) => {
+          // json is the JSON representation of the MIDI file.
+          console.log(json); // You can do something with the parsed MIDI data here.
+          setFileJSON(json);
+        });
+      };
+      reader.readAsArrayBuffer(selectedFile);
+    }
+  };
 
 	return(
    <div className="flex flex-row">
 			<input type="file" name="file" accept='.midi, .mid' onChange={changeHandler}/>
-			{isSelected ? (
-				<div className=""></div>
-			) : (
-				<p></p>
-			)}
-			<div className=" pl-8 pt-[3px]">
-				<button onClick={handleSubmission}>Submit</button>
+			<div className="h-8 pl-8 pt-[3px]">
+				<button onClick={handleFileChange}>Submit</button>
 			</div>
 		</div>
 	)
 }
 
-function Application() {
+function Application({ selectedFile, setSelectedFile, fileJSON, setFileJSON }) {
   return(
     <div className="flex flex-row">
       <div className="w-1/6"></div>
@@ -70,11 +76,14 @@ function Application() {
           <div className="w-full h-24 pt-12 flex flex-initial justify-center">
             <h1 className="text-xl">React File Upload</h1>
           </div>
-          <div className="w-full h-24 flex flex-initial justify-center">
-            <FileUploadPage/>
+          <div className="w-full h-8 flex flex-initial justify-center">
+            <FileUploadPage selectedFile={selectedFile} setSelectedFile={setSelectedFile} setFileJSON={setFileJSON}/>
           </div>
+          <div className="h-16"></div>
         </HeroPattern>
-        <div className="w-full h-64 flex flex-initial justify-center">
+        <div className="w-full h-16 flex flex-initial justify-center"></div>
+        <div className="w-full h-96 flex flex-initial justify-center overflow-auto text-white">
+          <pre className="w-full flex flex-initial">{JSON.stringify(fileJSON, null, 2)}</pre>
         </div>
       </div>
       <div className="w-1/6"></div>
@@ -83,12 +92,13 @@ function Application() {
 }
   
 function Body(){
-  
+  const [selectedFile, setSelectedFile] = useState();
+  const [fileJSON, setFileJSON] = useState();
   return (
     <>
       <NavBar/>
       <div className="bg-gradient-to-b from-gray-900 to-gray-600 bg-gradient-to-r pb-12">
-        <Application/>
+        <Application selectedFile={selectedFile} setSelectedFile={setSelectedFile} fileJSON={fileJSON} setFileJSON={setFileJSON}/>
         <Footer/>
       </div>
     </>
