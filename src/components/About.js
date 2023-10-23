ce2cdf0fbf2826135dec15c7ca876d0450a516be
Sandilya Kambhampati <63815093+kprsandilya@users.noreply.github.com>
2023-10-23 00:03:24 -0400

    Added visuals to make the website nicer.

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,24 @@ import '../App.css';
 import "../input.css";
 import NavBar from "./NavBar.js";
 import Footer from "./Footer.js";
+import HeroPattern from "./HeroPattern.js";
 
 function About() {
   return(
-      <div className="w-full h-48 flex flex-initial justify-center">
-          This is the about
+    <div className="flex flex-row">
+    <div className="w-1/6"></div>
+    <div className="text-slate-600 w-2/3">
+      <div className="h-8"></div>
+      <HeroPattern pttrn={'morphing-diamonds'}>
+        <div className="flex flex-col space-y-4">
+          <div className="w-full h-96 flex flex-initial justify-center pt-44">
+            <p>This is the about</p>  
+          </div>
+        </div>
+      </HeroPattern>
       </div>
+    <div className="w-1/6"></div>
+  </div>
   );
 }
   
--- src/components/Acknowledgements.js
+++ src/components/Acknowledgements.js
@@ -3,17 +3,33 @@ import '../App.css';
 import "../input.css";
 import NavBar from "./NavBar.js";
 import Footer from "./Footer.js";
+import HeroPattern from "./HeroPattern.js";
 
 function Acknowledgements() {
     return(
-      <>
-        <div className="w-full h-48 flex flex-initial justify-center">
-          <p>https://hypercolor.dev/</p>  
+    <div className="flex flex-row">
+      <div className="w-1/6"></div>
+      <div className="text-slate-600 w-2/3">
+        <div className="h-8"></div>
+        <HeroPattern pttrn={'temple'}>
+          <div className="flex flex-col space-y-4">
+            <div className="w-full h-20 flex flex-initial justify-center pt-8">
+              <p>https://hypercolor.dev/</p>  
+            </div>
+            <div className="w-full h-20 flex flex-initial justify-center pt-8">
+              <p>https://heropatterns.com/</p>
+            </div>
+            <div className="w-full h-20 flex flex-initial justify-center pt-8">
+              <p>https://tonejs.github.io/Midi/</p>
+            </div>
+            <div className="w-full h-28 flex flex-initial justify-center pt-8">
+              <p>https://magenta.github.io/magenta-js/music/</p>
+            </div>
+          </div>
+        </HeroPattern>
         </div>
-        <div className="w-full h-48 flex flex-initial justify-center">
-          <p>https://heropatterns.com/</p>
-        </div>
-      </>
+      <div className="w-1/6"></div>
+    </div>
     );
 }
   
