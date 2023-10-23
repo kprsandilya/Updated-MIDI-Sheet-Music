69b95707cb7ec5577341844ea06c3bae0318a029
Sandilya Kambhampati <63815093+kprsandilya@users.noreply.github.com>
2023-10-22 20:40:10 -0400

    Visualization working. Tracks need to be modified

--- src/components/staff_index.js
+++ src/components/staff_index.js
@@ -4,76 +4,109 @@ import * as ssv from './staff_svg_visualizer.ts'; // Import your staff_svg_visua
 
 const MidiVisualizerComponent = ({ noteSequences, number }) => {
   const staffRightRef = useRef(null);
-  
+  const playerRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
 
-  const STAFF_USE_CASES = {
-    notes: [
-      {pitch: 65, startTime: 3, endTime: 4, program: 0, velocity: 128},
-      {pitch: 65, startTime: 4, endTime: 8, program: 0, velocity: 128},
-      {pitch: 65, startTime: 12, endTime: 14, program: 0, velocity: 112},
-      {pitch: 65, startTime: 16, endTime: 17, program: 0, velocity: 96},
-      {pitch: 65, startTime: 18, endTime: 18.5, program: 0, velocity: 80},
-      {pitch: 65, startTime: 19, endTime: 19.25, program: 0, velocity: 64},
-      {pitch: 65, startTime: 19.5, endTime: 19.625, program: 0, velocity: 48},
-      {pitch: 65, startTime: 19.75, endTime: 19.8125, program: 0, velocity: 32},
-      {pitch: 77, startTime: 19.875, endTime: 20.0625, program: 0},
-      {pitch: 65, startTime: 24, endTime: 26, program: 0},
-      {pitch: 77, startTime: 25, endTime: 27, program: 0},
-      {pitch: 65, startTime: 29, endTime: 29.5, program: 0},
-      {pitch: 66, startTime: 29.5, endTime: 30, program: 0},
-      {pitch: 66, startTime: 30, endTime: 30.5, program: 0},
-      {pitch: 65, startTime: 30.5, endTime: 31, program: 0},
-      {pitch: 65, startTime: 31, endTime: 31.5, program: 0},
-      {pitch: 66, startTime: 31.5, endTime: 32, program: 0},
-      {pitch: 66, startTime: 32, endTime: 36.5, program: 0},
-      {pitch: 66, startTime: 36.5, endTime: 37, program: 0},
-      {pitch: 66, startTime: 37, endTime: 40, program: 0},
-    ],
-    tempos: [{time: 0, qpm: 60}],
-    keySignatures: [{time: 0, key: 0}],
-    timeSignatures: [{time: 0, numerator: 4, denominator: 4}],
-    totalTime: 40
-  };
-
-  //const parsedNoteSequence = STAFF_USE_CASES;
-  const parsedNoteSequence = noteSequences[0];
-  console.log(parsedNoteSequence);
+  const parsedNoteSequence = noteSequences[number];
 
   useEffect(() => {
-      if (parsedNoteSequence) {
-        const UNIFORM_TIME_SIZE = 200;
-        let visualizerRight;
+    if (parsedNoteSequence) {
+      const configRight = {
+        noteHeight: 15,
+        pixelsPerTimeStep: 0,
+        instruments: [0],
+        defaultKey: 7,
+        scrollType: ssv.ScrollType.BAR
+      };
 
-        const sequence1 = parsedNoteSequence;
-        const player1 = new mm.Player(false, {
-          run: (note) => {
-            visualizerRight.redraw(note, true);
-          },
-          stop: () => {}
-        });
+      const visualizerRight = new ssv.StaffSVGVisualizer(
+        parsedNoteSequence,
+        staffRightRef.current,
+        configRight
+      );
 
-        let configRight = {
-          noteHeight: 15,
-          pixelsPerTimeStep: 0,
-          instruments: [0],
-          defaultKey: 7,
-          scrollType: ssv.ScrollType.BAR
-        };
-
-        function initPlayerAndVisualizer(seq) {
-          visualizerRight = new ssv.StaffSVGVisualizer(seq, staffRightRef.current, configRight);
-
-          const tempo = 60;
-          player1.setTempo(tempo);
+      const player = new mm.Player(false, {
+        run: (note) => {
+          visualizerRight.redraw(note, true);
+        },
+        stop: () => {
+          setIsPlaying(false);
         }
+      });
 
-        initPlayerAndVisualizer(sequence1);
+      playerRef.current = player;
+
+      // Set initial tempo
+      const initialTempo = parsedNoteSequence.tempos[0].qpm;
+      player.setTempo(initialTempo);
+
+      // Cleanup when component unmounts
+      return () => {
+        player.stop();
+      };
+    }
+  }, [noteSequences]);
+
+  const handlePlay = () => {
+    const player = playerRef.current;
+    if (player) {
+      if (isPaused) {
+        player.resume(parsedNoteSequence);
+        setIsPlaying(true);
+        setIsPaused(false);
+      } else {
+        player.start(parsedNoteSequence);
+        setIsPlaying(true);
+        setIsPaused(false);
       }
-  }, [noteSequences]); // Re-run effect when the midiFile prop changes
+    }
+  };
+
+  const handlePause = () => {
+    const player = playerRef.current;
+    if (player) {
+      player.pause();
+      setIsPlaying(false);
+      setIsPaused(true)
+    }
+  };
+
+  const handleStop = () => {
+    const player = playerRef.current;
+    if (player) {
+      player.stop();
+      setIsPlaying(false);
+      setIsPaused(false);
+    }
+  };
 
   return (
-    <div>
+    <div className="overflow-auto w-full flex flex-col space-y-4">
       <div ref={staffRightRef}></div>
+      <div className="flex flex-row space-x-8">
+        <div className="flex flex-row w-1/3">
+            <div className="w-1/6"></div>
+            <div className="w-2/3">
+            <button onClick={handlePlay} disabled={isPlaying} className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Play</button>
+            </div>
+            <div className="w-1/6"></div>
+        </div>
+        <div className="flex flex-row w-1/3">
+            <div className="w-1/6"></div>
+            <div className="w-2/3">
+            <button onClick={handlePause} disabled={!isPlaying} className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Pause</button>
+            </div>
+            <div className="w-1/6"></div>
+        </div>
+        <div className="flex flex-row w-1/3">
+            <div className="w-1/6"></div>
+            <div className="w-2/3">
+            <button onClick={handleStop} disabled={!isPlaying} className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Stop</button>
+            </div>
+            <div className="w-1/6"></div>
+        </div>
+      </div>
     </div>
   );
 };
